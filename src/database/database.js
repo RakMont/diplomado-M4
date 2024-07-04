8fac704c637ea96c7def0ef2a0ae5cb681d4d62b
RakMont <nabogaria@gmail.com>
2024-07-04 19:51:48 -0400

    fix ssl

--- src/database/database.js
+++ src/database/database.js
@@ -12,6 +12,7 @@ export const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         port: process.env.DB_PORT,
-        logging: console.log
+        logging: console.log,
+        ssl: true
     }
 );
